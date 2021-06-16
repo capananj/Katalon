4143fdc07dd58fcaa96efdd396b0112b7b1ed740
Jett Liu <jett.l@clubware.co.nz>
2021-06-17 11:12:26 +1200

    initial commit

---
+++ My First Web UI Project/Object Repository/Makes/getMake.rs
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebServiceRequestEntity>
+   <description></description>
+   <name>getMake</name>
+   <tag></tag>
+   <elementGuidId>e72794f8-13c1-415b-82dd-0aa5061f97bc</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <connectionTimeout>0</connectionTimeout>
+   <followRedirects>false</followRedirects>
+   <httpBody></httpBody>
+   <httpBodyContent></httpBodyContent>
+   <httpBodyType></httpBodyType>
+   <httpHeaderProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>Authorization</name>
+      <type>Main</type>
+      <value>Bearer eyJraWQiOiJ3Vm1QOTZNZk03K2UzRmRzWHJ0TXNVMjl0dGN1R0IrMEJ3Z3RKZDBRT3lrPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI2OWY5YTU3ZS0zNGExLTQzYTYtODJlZS04N2NmYjQ5ZDIxNDYiLCJldmVudF9pZCI6IjNlYmI1NTg0LTUzNDMtNGIwMi04NjU0LWE0NTY5N2NkMmEwZCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4iLCJhdXRoX3RpbWUiOjE2MjM4MzYzMDQsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMi5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMl9FRk5XN3V3V2giLCJleHAiOjE2MjM4Mzk5MDQsImlhdCI6MTYyMzgzNjMwNCwianRpIjoiZTMyYTFjMzEtM2MwOC00MzRmLTlkYjQtNGFjYjFkOWRmYjc2IiwiY2xpZW50X2lkIjoiM2RrcjJsNTc3MjFocWdlNmxwZmxzZGcxcmEiLCJ1c2VybmFtZSI6InRlc3RhYWEifQ.RvD4YPdYuOMw2UpXCyC6sYcbhHAqpm7lQD2waXCpf9S_9EpLg9trwNNWXq46-Bs0rOHabe6PlHpkkmnESL0YzDkkhGJNA80Cr5KVQ6fFBxK52XmMAiG4oUNJAEOlhKAd2WCVsyPbDPqUYgzqcNXb8ixADI70V_KwXNb7gIhBksAK9ImJeJVXynTPHvAVaAU5CepX235L9cqSYFtJMeGcpihGtR_p36kERZj0Laaq0uKIbwpaMvpV-eJoKkBsdi1z8Ra2tylZ1xzh1eZkwTljYyYz4ReS5TfTgFVidNH_9g0SJQ1izrLtAS_bSPM9uwh37AhTDkWuths2aIYcQiYDzw</value>
+   </httpHeaderProperties>
+   <katalonVersion>8.0.5</katalonVersion>
+   <maxResponseSize>0</maxResponseSize>
+   <migratedVersion>5.4.1</migratedVersion>
+   <restRequestMethod>GET</restRequestMethod>
+   <restUrl>https://k51qryqov3.execute-api.ap-southeast-2.amazonaws.com/prod/makes/c0bm09bgagshpkqbsuag</restUrl>
+   <serviceType>RESTful</serviceType>
+   <soapBody></soapBody>
+   <soapHeader></soapHeader>
+   <soapRequestMethod></soapRequestMethod>
+   <soapServiceEndpoint></soapServiceEndpoint>
+   <soapServiceFunction></soapServiceFunction>
+   <socketTimeout>0</socketTimeout>
+   <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.RequestObject
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+
+RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
+
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+   <wsdlAddress></wsdlAddress>
+</WebServiceRequestEntity>
